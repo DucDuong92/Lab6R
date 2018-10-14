ccba4f9de105cb4db83e20d754955b51b8cd47d5
DucDuong92 <minhducduong92@gmail.com>
2018-10-14 23:28:59 +0200

    Add parallel processing

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -6,12 +6,13 @@
 #'
 #' @param x , is the input weight
 #' @param W , is the input value
+#' @param parallel, using parallel processing or not
 #'
 #'
 #' @return list , returns a list that contain the maximum value and the list of elements chosen
 #' @export
 
-brute_force_knapsack <- function(x, W)
+brute_force_knapsack <- function(x, W, parallel = FALSE)
 {
   #check the input
   if(is.data.frame(x)==FALSE)
@@ -24,7 +25,10 @@ brute_force_knapsack <- function(x, W)
   #++++++++++++++++++++++++++++++++
     value <- 0
     elements <- c()
-    for (i in 1:length(x$v)) {
+    length <- length(x$v)
+    if (parallel==FALSE) {
+
+    for (i in 1:length) {
       #scan the input value, combine all posible cases
       combine_w<-as.data.frame(combn(x[,1], i))
       combine_v<-as.data.frame(combn(x[,2], i))
@@ -54,9 +58,63 @@ brute_force_knapsack <- function(x, W)
     # This loop make the systerm run slowly
     #  to pass the test suite "expect_true(as.numeric(st)[2] > 0.00)"
     #  maybe because my computer is fast...
-    for (i in 10000) {
-      i <- i+1
+    #for (i in 10000) {
+    #  i <- i+1
+    #  }
     }
+    else {
+
+      #In the parallel version, I use all cores to calculate the combination matrix.
+      #In my opinion, this is the most time comming part of this function.
+
+      library(parallel)
+      numCores <- detectCores()
+      cl <- makeCluster(numCores)
+
+      f1 <- function(i) {
+        combine_w<-combn(x[,1], i)
+      }
+
+      f2 <- function(i) {
+        combine_w<-combn(x[,2], i)
+      }
+
+      # Parallel processing by parLapply
+      combine_w_list <- parLapply(cl, 1:length, f1)
+      combine_v_list <- parLapply(cl, 1:length, f2)
+
+      stopCluster(cl)
+
+      for (i in 1:length) {
+
+        combine_w <- as.data.frame(combine_w_list[[i]])
+        combine_v <- as.data.frame(combine_v_list[[i]])
+
+        sumw<-colSums(combine_w)
+        sumv<-colSums(combine_v)
+
+
+        #take the cases that have w <= input W
+        components <- which(sumw<=W)
+        #remove the empty case
+        if(length(components) != 0){
+          #print(components)
+
+          #take the max value of this case, assign to value_temp
+          values <- sumv[components]
+          value_temp <- max(values)
+
+          #compare with the global max value
+          if (value_temp > value) {
+            value <- value_temp
+            index <- which(sumv == value)
+            weight <- combine_w[,index]
+            elements <- which(x$w == weight)
+          }
+        }
+      }
+
+  }
 
     output <- list("value" = round(value), "elements" = elements)
     return(output)
